753fad0efe2cd1722e5cc127b3856375559b3d5d
ethuringer <ethuringer@gmail.com>
2020-11-19 18:21:04 +0100

    transition ready

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 (function visibleModal(event) {
     const modal = document.querySelector('.modal');
-    const btn = document.querySelector(".btn__main");{
+    const btn = document.querySelector('.btn__main');{
         btn.addEventListener('click', (event) =>
-        modal.style.visibility = "visible");
+        modal.className = 'modal__visible');
         
     }
 })();
@@ -12,13 +12,13 @@
     const closeElements = document.querySelectorAll('.close__button, .btn__modal--green, .btn__modal--red');
         for (let i = 0; i < closeElements.length; i += 1) {
         closeElements[i].addEventListener('click', (event) =>
-        modal.style.visibility = "hidden");
+        modal.className = 'modal');
 }
 })();
 
 const modal = document.querySelector('.modal');
 window.addEventListener('click', function (event){
     if (event.target == modal) {
-        modal.style.visibility = "hidden";
+        modal.className = 'modal';
     }
 });
