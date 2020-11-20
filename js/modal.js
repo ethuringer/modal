8d22a46c3d1b1bb8cd011de6efdc21d572c3037c
ethuringer <ethuringer@gmail.com>
2020-11-20 23:38:24 +0100

    overlay positioning

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@
     const modal = document.querySelector('.modal');
     const closeElements = document.querySelectorAll('.close__button, .btn__modal--green, .btn__modal--red, .modal__overlay');
         for (let i = 0; i < closeElements.length; i += 1) {
+            // console.log(closeElements[i]);
         closeElements[i].addEventListener('click', () =>
         modal.classList.remove('modal__visible'));
 }
