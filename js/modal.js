04e1fe9b002e1361e9b8869af92302df1cf74e88
ethuringer <ethuringer@gmail.com>
2020-11-21 16:33:58 +0100

    overlay final css

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,24 @@
 (function visibleModal() {
     const modal = document.querySelector('.modal');
+    const modalOverlay = document.querySelector('.modal__overlay');
     const btn = document.querySelector('.btn__main');{
-        btn.addEventListener('click', () =>
-        modal.classList.add ('modal__visible'));
-        
+        btn.addEventListener('click', () => {
+        modal.classList.add ('modal__visible'),
+        modalOverlay.classList.add('overlay__visible')
+    });
     }
 })();
 
 (function hideModal() {
     const modal = document.querySelector('.modal');
+    const modalOverlay = document.querySelector('.modal__overlay');
     const closeElements = document.querySelectorAll('.close__button, .btn__modal--green, .btn__modal--red, .modal__overlay');
         for (let i = 0; i < closeElements.length; i += 1) {
             // console.log(closeElements[i]);
-        closeElements[i].addEventListener('click', () =>
-        modal.classList.remove('modal__visible'));
+        closeElements[i].addEventListener('click', () => {
+        modal.classList.remove('modal__visible'),
+        modalOverlay.classList.remove('overlay__visible')
+    });
 }
 })();
 
